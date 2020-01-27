ecb875e1aae9ba1fa3846d33933d74de56b75a02
filipgermanek <filip@forecast.it>
2020-01-27 15:59:12 +0100

    exercise 1 part 2 completed

--- ex_1/lib/producer_consumer.ex
+++ ex_1/lib/producer_consumer.ex
@@ -11,8 +11,18 @@ defmodule AdventOfCode.ProducerConsumer do
     {:producer_consumer, state, subscribe_to: [AdventOfCode.Producer]}
   end
 
+  def calculateFuel(mass) do
+    result = Integer.floor_div(mass, 3) - 2
+    if (result < 0) do
+      0
+    else
+      result + calculateFuel(result)
+    end
+  end
+
   def handle_events(inputValues, _from, state) do
-    events = [Enum.map(inputValues, fn x -> Integer.floor_div(x, 3) - 2 end) |> Enum.sum]
+    # temp = [1969]
+    events = [Enum.map(inputValues, &calculateFuel/1) |> Enum.sum]
     {:noreply, events, state}
   end
 end
